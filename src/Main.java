ccd1c37acd13a35d65db1aed436ece40eee83a54
ludwigpersson <perssonludwig1@gmail.com>
2023-12-11 13:40:37 +0100

    Skapat menyer

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // test2
-        // test/yrla
-        // test/Ludwig1
-        // test Thai
-        // test Trung
+
+        printFirstMenu();
+
+
     }
 }
